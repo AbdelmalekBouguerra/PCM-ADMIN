bddb57d2477b69eef7229127b00acdaebf965fd1
AbdelmalekBouguerra <abdelmalekbouguerra2000@gmail.com>
2022-02-07 23:04:36 +0100

    setting the app

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const dotenv = require('dotenv');
+const hbs = require('hbs');
+const path = require('path');
+// env config
+dotenv.config();
+
+// server config
+const app = express();
+// cert and key for https server
+const key = fs.readFileSync(process.env.CERT_KEY);
+const cert = fs.readFileSync(process.env.CERT);
+
+var httpsServer = https.createServer({key: key, cert: cert}, app);
+const port = process.env.PORT;
+
+httpsServer.listen(port,()=>{
+    console.log(`Server started : https://localhost:${port}`);
+})
+
+// Parse URL encoded bodies sent by forms
+app.use(express.urlencoded({ extended:false}))
+// Parse JSON bodies as sent by API clients
+app.use(express.json())
+
+// define public directory
+const publicDirectory = path.join(__dirname,'./public')
+app.use(express.static(publicDirectory))
+
+// Define view engine
+app.set('view engine', 'hbs');
+
+// template hbs
+hbs.registerPartials(path.join(__dirname, 'views', 'templates'))
+
+// Routers
+const indexRouter = require('./routes/index');
+
+app.use('/',indexRouter);
