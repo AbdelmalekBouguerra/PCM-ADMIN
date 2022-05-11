48b00b7deb51c3f2b0d4daeb838cbd3f84ce3e93
AbdelmalekBouguerra <abdelmalekbouguerra2000@gmail.com>
2022-05-11 20:08:26 +0100

    adding pc table

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const dpc = require("../controllers/dpc_table");
 const express = require("express");
 const res = require("express/lib/response");
 const mc_table = require("../controllers/mc_table");
+const pc_table = require("../controllers/pc_table");
 const router = express.Router();
 
 var user;
@@ -43,7 +44,7 @@ router.get("/shtab", (req, res) => {
 });
 // to get sh table
 router.route("/sh").get(emp_table.get).post(emp_table.post);
-
+// MEDECINS_CONVENTIONNES table routes====================
 router.get("/Medecins_conventionnes", (req, res) => {
   user = req.session.user;
   Fl = user.PRENOM.charAt(0).toUpperCase();
@@ -53,11 +54,23 @@ router.get("/Medecins_conventionnes", (req, res) => {
     LastName: Ln,
   });
 });
-
 router.route("/MC").get(mc_table.get).post(mc_table.post);
 router.post("/deleteMC", mc_table.delete);
+// -------------------------------------------------------
 
-
+// PRESTATIONS_CMS =======================================
+router.get("/Prestations_cms", (req, res) => {
+  user = req.session.user;
+  Fl = user.PRENOM.charAt(0).toUpperCase();
+  Ln = user.NOM.charAt(0).toUpperCase() + user.NOM.slice(1);
+  res.render("PC", {
+    FirstName: Fl,
+    LastName: Ln,
+  });
+});
+router.route("/PC").get(pc_table.get).post(pc_table.post);
+router.post("/deletePC", pc_table.delete);
+// -------------------------------------------------------
 router.get("/DPC", (req, res) => {
   res.render("DPC");
 });
